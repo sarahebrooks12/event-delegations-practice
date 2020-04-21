9dcd09ad72cb6a6949bcd6a3d3a517998348b0ea
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-04-21 11:32:06 -0400

    one function to rule them all

---
+++ script.js
@@ -0,0 +1,27 @@
+// Practice: These are a few of my favorite things
+// Build two fieldsets and input fields that will allow you to enter the name of a favorite thing of yours, and a location where you might purchase it (Target, Amazon, Best Buy, etc...)
+
+// Add a button beneath the fieldsets labeled "Save to Wishlist".
+
+// When the button is clicked, display the data in the following format in the DOM.
+
+// I can purchase {thing} at {location}
+
+
+
+
+// Practice: SPA Navigation Bar
+// Setup
+// Use the basicProj command to create a new project with an index.html file, a script file, and a CSS sheet.
+
+// Requirements
+// In your index.html file, create a navigation bar with four links:
+// "Home"
+// "Contact Me"
+// "About Me"
+// "Projects"
+// When the user clicks on any given link, they should see an h1 element with an appropriate heading and some lorem ipsum text. For example, when the user clicks on the "Home" link, they should see a heading that says "Home" and some dummy text.
+// All content should be rendered and removed dynamically with JavaScript
+// You should only have one HTML document.
+// In the "Contact Me" section, the user should see a form with two inputs: one for the user's name and one for their message. They should also see a button that says "Send".
+// When the user clicks the "Send" button, the message inputs should clear and they should see a confirmation message below the form that says "Your message has been sent"
